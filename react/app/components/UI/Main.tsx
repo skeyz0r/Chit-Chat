62a3a09dee3659757af10a88e2aa45826a08fef7
Shon Danvudi <sdanvudi@gmail.com>
2024-02-15 17:47:20 -0600

    Update Main.tsx

--- react/app/components/UI/Main.tsx
+++ react/app/components/UI/Main.tsx
@@ -18,10 +18,10 @@ export default function Main(info:{value:string, username:string | undefined, id
 const [text, setText] = useState('')
 const [loaded, setLoaded] = useState<message[]>([])
 const [newMsg, setNew] = useState(false)
-const endRed = useRef<HTMLInputElement>(null)
+const endRef = useRef<HTMLInputElement>(null)
 
 const scrollToEnd = () => {
-    endRed.current?.scrollIntoView({behavior: "smooth"})
+    endRef.current?.scrollIntoView({behavior: "smooth"})
 }
 
 useEffect(()=>{
@@ -46,6 +46,10 @@ return()=>{
 
 },[ info.chatId])
 
+useEffect(()=>{
+    scrollToEnd()
+},[loaded])
+
 function setMessage(response:any)
 {
     if(response.answer.length === 0)
@@ -54,7 +58,6 @@ function setMessage(response:any)
     }
     for(let i = 0; i < response.answer.length; i++)
     {
-        console.log('Answer', response.answer[i].sender)
         setLoaded(oldArray =>[...oldArray, {text: response.answer[i].text, date:response.answer[i].date, sender:response.answer[i].sender}]) 
  }}
 
@@ -63,13 +66,16 @@ const messageHandler = (text:string)=>{
     setLoaded(prevLoaded => [
         ...prevLoaded,
         { text: text, date: Date.now().toString(), sender: Number(info.id)},
-      ]);}
+      ]);
+    alert(info.id)
+    }
+
 
 
 async function newMessage()
 {
     const chatId = info.chatId
-    const sender = info.authordId
+    const sender = info.id
     setNew(false)
    fetch('/api/newmessage', {method: 'POST',
    body: JSON.stringify({text, chatId, sender})})
@@ -81,23 +87,20 @@ async function newMessage()
 
     return(
         info.value === 'news' ? <News true={true} authorId={info.authordId} name={info.username}/> :
-       
         info.value === 'news' ? <News true={true} authorId={info.authordId} name={info.username}/> : info.value === 'profile' ? <News authorId={info.authordId} true={false} name={info.username}/>  : info.value === '' ?  <News authorId={info.authordId} true={true} name={info.username}/> :
         <div className="flex  w-full flex-col">
         <h2 className={`p-2 border-md text-2xl w-full flex justify-center items-center border `}>{info.value}</h2>
-    
 <div  className={`${loaded.length === 0 ? 'justify-center' : ''} px-4 flex overflow-y-scroll flex-col h-[85%]`}>
     { newMsg ? <p className="self-center">Start the chit chat in {info.value}</p> :
         loaded.length === 0 ? <div className="self-center items-center flex gap-4"><p className="text-3xl uppercase">Loading</p>
    <AiOutlineLoading3Quarters size={30} id="spinner"/></div> :
-    
-
 loaded.map((data, key) =>{
     return(
 <Message id={String(key)} text={data.text} date={data.date} key={key} sender={data.sender === info.authordId ? true : false}/>
 )
 })
 }
+<div ref={endRef}></div>
 </div>
 <div className="bottom-0 w-full items-center self-end flex justify-evenly border-t mb-4">
     <textarea value={text} onChange={(e)=>{setText(e.currentTarget.value)}} className="border my-3 w-[70%] h-32 py-4 resize-none p-2 text-black outline-none rounded-lg" placeholder="Say hi!"/>
